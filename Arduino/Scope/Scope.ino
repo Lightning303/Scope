df1974520cb4e0b55b1f7f843f6bf827b65cabe9
Lightning303 <Lightning303@gmail.com>
2015-02-12 23:11:26 +0100

    Changed from Adafruit_ILI9340 library to ILI9341_due because it is significantly faster

--- Arduino/Scope/Scope.ino
+++ Arduino/Scope/Scope.ino
@@ -1,6 +1,9 @@
-#include "SPI.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9340.h"
+#include <SPI.h>
+#include <ILI_SdSpi.h>
+#include <ILI_SdFatConfig.h>
+#include <ILI9341_due_gText.h>
+#include <ILI9341_due.h>
+#include "fonts\Verdana18.h"
 
 #define WIDTH 320
 #define HEIGHT 240
@@ -15,9 +18,8 @@
 
 #define divSize 32
 
-Adafruit_ILI9340 tft = Adafruit_ILI9340(CS, DC, RST);
-
 // Color definitions
+// redundant!
 #define	BLACK   0x0000
 #define	BLUE    0x001F
 #define	RED     0xF800
@@ -27,39 +29,52 @@ Adafruit_ILI9340 tft = Adafruit_ILI9340(CS, DC, RST);
 #define YELLOW  0xFFE0  
 #define WHITE   0xFFFF
 
+// ILI9341_due lib init
+ILI9341_due tft = ILI9341_due(CS, DC, RST);
+ILI9341_due_gText vMaxArea(&tft);
+ILI9341_due_gText vMinArea(&tft);
+ILI9341_due_gText timePerDivArea(&tft);
+ILI9341_due_gText frequencyArea(&tft);
 
+// buffer
 byte valueBuffer[WIDTH][2];
-int valueIndex = 0;
-byte bufferIndex = 0;
 byte vMax[2];
 byte vMin[2];
 unsigned long timeMessured[2];
 float frequency[2];
+
+// indexes
+int valueIndex = 0;
+byte bufferIndex = 0;
+
 boolean fillScreen = false;
-
-int digitalDelay;
-
-int poti = 0;
-int triggerLevel = 512;
 boolean digitalMode = true;
-
-
+int digitalDelay;
 float steps;
 
+// temp
+int poti = 0;
+int triggerLevel = 512;
 
 void setup()
 {
-  vMax[1] = 255;
-  vMin[1] = 0;
   // sanity check
   delay(5000);
+  
   Serial.begin(115200);
+  
   pinMode(ANALOG, INPUT);
   pinMode(DIGITAL, INPUT);
   pinMode(DIGITALMODE, INPUT);
+  
+  // temp? signal output (pwm 50%)
   pinMode(3, OUTPUT);
   analogWrite(3, 128);
+  
   setupTFT();
+  
+  vMax[1] = 255;
+  vMin[1] = 0;
 }
 
 
@@ -121,8 +136,29 @@ void loop()
 void setupTFT()
 {
   tft.begin();
-  tft.setRotation(3);
+  tft.setRotation(iliRotation270);
   tft.fillScreen(BLACK);
+  
+  vMaxArea.defineArea(8, (6 * divSize) + 6, WIDTH / 2, (6 * divSize) + ((HEIGHT - (6 * divSize)) / 2));
+  vMaxArea.selectFont(Verdana18);
+  vMaxArea.setFontLetterSpacing(2);
+  vMaxArea.setFontColor(WHITE, BLACK);
+  
+  vMinArea.defineArea(8, (6 * divSize) + ((HEIGHT - (6 * divSize)) / 2), WIDTH / 2, HEIGHT);
+  vMinArea.selectFont(Verdana18);
+  vMinArea.setFontLetterSpacing(2);
+  vMinArea.setFontColor(WHITE, BLACK);
+  
+  timePerDivArea.defineArea(WIDTH / 2, (6 * divSize) + 6, WIDTH, (6 * divSize) + ((HEIGHT - (6 * divSize)) / 2));
+  timePerDivArea.selectFont(Verdana18);
+  timePerDivArea.setFontLetterSpacing(2);
+  timePerDivArea.setFontColor(WHITE, BLACK);
+  
+  frequencyArea.defineArea(WIDTH / 2, (6 * divSize) + ((HEIGHT - (6 * divSize)) / 2), WIDTH, HEIGHT);
+  frequencyArea.selectFont(Verdana18);
+  frequencyArea.setFontLetterSpacing(2);
+  frequencyArea.setFontColor(WHITE, BLACK);
+
   drawGUI();
 }
 
@@ -142,11 +178,11 @@ void updateTFT()
 {
   if (fillScreen)
   {
-    tft.fillRect(0, 0, WIDTH, (6 * divSize) + 2, BLACK);
+    //tft.fillRect(0, 0, WIDTH, (6 * divSize) + 2, BLACK);
   }
   else
   {
-    removeGraph();
+    //removeGraph();
   }
   drawGUI();
   
@@ -155,6 +191,11 @@ void updateTFT()
 
   for (int i = 1; i < (int)steps; i++)
   {
+    // temp playing around with continious waves
+    tft.drawLine((i * stepSize) - stepSize, valueBuffer[i - 1][bufferIndex == 0 ? 1 : 0] - 1, i == floor(steps - 1) ? WIDTH : i * stepSize, valueBuffer[i][bufferIndex == 0 ? 1 : 0] - 1, BLACK);
+    tft.drawLine((i * stepSize) - stepSize, valueBuffer[i - 1][bufferIndex == 0 ? 1 : 0], i == floor(steps - 1) ? WIDTH : i * stepSize, valueBuffer[i][bufferIndex == 0 ? 1 : 0], BLACK);
+    tft.drawLine((i * stepSize) - stepSize, valueBuffer[i - 1][bufferIndex == 0 ? 1 : 0] + 1, i == floor(steps - 1) ? WIDTH : i * stepSize, valueBuffer[i][bufferIndex == 0 ? 1 : 0] + 1, BLACK);
+    
     tft.drawLine((i * stepSize) - stepSize, valueBuffer[i - 1][bufferIndex] - 1, i == floor(steps - 1) ? WIDTH : i * stepSize, valueBuffer[i][bufferIndex] - 1, digitalMode ? GREEN : RED);
     tft.drawLine((i * stepSize) - stepSize, valueBuffer[i - 1][bufferIndex], i == floor(steps - 1) ? WIDTH : i * stepSize, valueBuffer[i][bufferIndex], digitalMode ? GREEN : RED);
     tft.drawLine((i * stepSize) - stepSize, valueBuffer[i - 1][bufferIndex] + 1, i == floor(steps - 1) ? WIDTH : i * stepSize, valueBuffer[i][bufferIndex] + 1, digitalMode ? GREEN : RED);
@@ -169,7 +210,8 @@ void updateTFT()
       vMin[0] = valueBuffer[i][bufferIndex];
     }
   }
-  if (millis() - stopwatch >= 1000)
+  // time it takes fillRect to clear graph
+  if (millis() - stopwatch > 208)
   {
     fillScreen = true;
   }
@@ -201,41 +243,39 @@ void updateTFT()
   {
     frequency[0] = 0;
   }
-      
-  removeText();
   
-  if (vMax[0] != vMax[1] && !digitalMode)
+  char tempString[20];
+  char tempValue[10];
+
+  if (vMax[0] != vMax[1])
   {
     vMax[1] = vMax[0];
-    writeMessure("Vmax: ", (float)((6 * divSize) - vMax[0]) / (6 * divSize) * 5, "V", 8, (6 * divSize) + 8, WHITE);
+    ftoa(tempValue, (float)((6 * divSize) - vMax[0]) / (6 * divSize) * 5, 2);
+    sprintf(tempString, "Vmax: %sV", tempValue);
+    vMaxArea.drawString(tempString, gTextAlignMiddleLeft, gTextEraseFullLine);
   }
-  if (vMin[0] != vMin[1] && !digitalMode)
+  if (vMin[0] != vMin[1])
   {
     vMin[1] = vMin[0];
-    writeMessure("Vmin: ", (float)((6 * divSize) - vMin[0]) / (6 * divSize) * 5, "V", 8, (6 * divSize) + 28, WHITE);
+    ftoa(tempValue, (float)((6 * divSize) - vMin[0]) / (6 * divSize) * 5, 2);
+    sprintf(tempString, "Vmin: %sV", tempValue);
+    vMinArea.drawString(tempString, gTextAlignMiddleLeft, gTextEraseFullLine);
   }
   if ((timeMessured[0] >= 10000 ? timeMessured[0] / 100 : timeMessured[0]) != (timeMessured[1] >= 10000 ? timeMessured[1] / 100 : timeMessured[1]))
   {
     timeMessured[1] = timeMessured[0];
-    writeMessure("", timeMessured[0] >= 10000 ? (float)timeMessured[0] / 10000 : (float)timeMessured[0] / 10, timeMessured[0] >= 10000 ? "mS/Div" : "uS/Div", 168, (6 * divSize) + 8, WHITE);
+    ftoa(tempValue, timeMessured[0] >= 10000 ? (float)timeMessured[0] / 10000 : (float)timeMessured[0] / 10, 2);
+    sprintf(tempString, timeMessured[0] >= 10000 ? "dt: %sms/Div" : "%sus/Div", tempValue);
+    timePerDivArea.drawString(tempString, gTextAlignMiddleLeft, gTextEraseFullLine);
   }
   if (frequency[0] != frequency[1])
   {
     frequency[1] = frequency[0];
-    writeMessure("f: ", frequency[0], "Hz", 168, (6 * divSize) + 28, WHITE);
+    sprintf(tempString, "f: %dHz", (int)frequency[0]);
+    frequencyArea.drawString(tempString, gTextAlignMiddleLeft, gTextEraseFullLine);
   }
   
-  delay(1000);
-}
-
-void writeMessure(String prefix, float value, String suffix, byte x, byte y, int color)
-{
-  tft.setTextColor(color);
-  tft.setTextSize(2);
-  tft.setCursor(x, y);
-  prefix += value;
-  prefix += suffix;
-  tft.println(prefix);
+  delay(1);
 }
 
 void removeGraph()
@@ -249,22 +289,16 @@ void removeGraph()
   }
 }
 
-void removeText()
+char *ftoa(char *a, double f, int precision)
 {
-  if (vMax[0] != vMax[1] || digitalMode)
-  {
-    writeMessure("Vmax: ", (float)((6 * divSize) - vMax[1]) / (6 * divSize) * 5, "V", 8, (6 * divSize) + 8, BLACK);
-  }
-  if (vMin[0] != vMin[1] || digitalMode)
-  {
-    writeMessure("Vmin: ", (float)((6 * divSize) - vMin[1]) / (6 * divSize) * 5, "V", 8, (6 * divSize) + 28, BLACK);
-  }
-  if ((timeMessured[0] >= 10000 ? timeMessured[0] / 100 : timeMessured[0]) != (timeMessured[1] >= 10000 ? timeMessured[1] / 100 : timeMessured[1]))
-  {
-    writeMessure("", timeMessured[1] >= 10000 ? (float)timeMessured[1] / 10000 : (float)timeMessured[1] / 10, timeMessured[1] >= 10000 ? "mS/DIV" : "uS/Div", 168, (6 * divSize) + 8, BLACK);
-  }
-  if (frequency[0] != frequency[1])
-  {
-    writeMessure("f: ", frequency[1], "Hz", 168, (6 * divSize) + 28, BLACK);
-  }
+ long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+ 
+ char *ret = a;
+ long heiltal = (long)f;
+ itoa(heiltal, a, 10);
+ while (*a != '\0') a++;
+ *a++ = '.';
+ long desimal = abs((long)((f - heiltal) * p[precision]));
+ itoa(desimal, a, 10);
+ return ret;
 }
